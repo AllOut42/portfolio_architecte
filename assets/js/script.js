3010f5eec9403ef236130ed2176ef0564fa513d8
Nathan Dancoine <dancoinathan@gmail.com>
2024-05-10 10:05:54 +0200

    filtre

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,12 +3,12 @@
 // 3 : Au clic sur un bouton trier les catégories
 
 async function getFilters() {
-  const response = await fetch("htttp://localhost:5678/api/categories");
+  const response = await fetch("http://localhost:5678/api/categories");
   const data = await response.json();
 
-  console.log(data);
-
   const filters = document.getElementById("filters");
+  const allButton = createFilterButton("0", "Tous");
+  filters.appendChild(allButton);
 
   data.forEach((item) => {
     const button = createFilterButton(item.id, item.name);
@@ -16,29 +16,64 @@ async function getFilters() {
   });
 
   filters.addEventListener("click", (event) => {
-    console.log(event);
     if (event.target.tagName === "BUTTON") {
+      const categoryId = Number(event.target.value);
+      getWorks(categoryId);
+      setActiveFilter(event.target);
     }
   });
 }
 
-async function getWorks() {
-  const response = await fetch("htttp://localhost:5678/api/works");
-  const data = await response.json();
+async function getWorks(categoryId = 0) {
+  try {
+    const response = await fetch("http://localhost:5678/api/works");
+    const data = await response.json();
 
-  console.log(data);
+    const gallery = document.querySelector(".gallery");
+    gallery.innerHTML = "";
 
-  // Afficher les works dans le DOM en respectant le template
-  /*
+    const filterData = data.filter(
+      (work) => work.categoryId === Number(categoryId) || categoryId === 0
+    );
 
-   <figure>
-            <img src="assets/images/abajour-tahina.png" alt="Abajour Tahina" />
-            <figcaption>Abajour Tahina</figcaption>
-   </figure>
+    const figures = filterData.map(({ id, imageUrl, title }) => {
+      const figure = document.createElement("figure");
+      figure.innerHTML += `
+      <img src="${imageUrl}" alt="${title}" />
+      <figcaption>${title}</figcaption>
+      `;
+      return figure;
+    });
 
-
-  */
+    figures.forEach((figure) => gallery.appendChild(figure));
+  } catch (error) {
+    console.error(error);
+  }
 }
 
-getFilters();
-getWorks();
+function createFilterButton(value, text) {
+  const button = document.createElement("button");
+  button.className = "filter-option";
+  button.textContent = text;
+  button.value = value;
+  return button;
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  getFilters().then(() => {
+    const allButton = document.querySelector('.filter-option[value="0"]');
+
+    if (allButton) {
+      allButton.classList.add("active");
+    }
+
+    getWorks(0).then(() => console.log("works loaded"));
+  });
+});
+
+function setActiveFilter(button) {
+  document
+    .querySelectorAll(".filter-option")
+    .forEach((b) => b.classList.remove("active"));
+  button.classList.add("active");
+}
